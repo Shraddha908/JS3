05efe32e4cffb76d74fad5c555cadeeee3bed47f
Shraddha More <moresatish305@gmail.com>
2025-09-20 12:37:40 +0530

    prob1

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-let main = document.getElementById("main")
-let api = "https://fakestoreapi.com/products";
 
-async function gatData(){
-  let res = await fetch(api)
-  let data = await res.json()
-
-data.map((el)=>{
-    console.log(el)
-    let name = document.createElement("h2")
-    name.innerText = el.title;
-    let price = document.createElement("h3")
-    price.innerText = el.price
-    let img = document.createElement("img")
-    img.src = el.image
-    let div = document.createElement("div")
-
-    div.append(img,name.price)
-
-    main.apppend(div)
-    
-  })
+function UserData(a,b) {
+  this.name = a,
+   this.age = b;
 }
+
+let a = new UserData("Rahul",22);
+let b = new UserData("Rahul1",23);
+let c = new UserData("Rahul2",24);
+let d = new UserData("Rahul3",25);
+
+let x = new Array()
+console.log(x)
+let objData = { }  
