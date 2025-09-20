0d72fe0eefba4e19883662e460062a8ddf08e78d
Shraddha More <moresatish305@gmail.com>
2025-09-20 13:59:37 +0530

    fethc
    fetch

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
+let main = document.getElementById("main")
+let filprice = document.querySelector("select")
 let input = document.querySelector("input")
-let img = document.querySelector("img")
-let password = 123456;
 
 
-function Checkfun(){
- let x = input.value;
- let promise = new Promise(function(res,rej){
-  if(password==x){
-    res("Password Match")
-    }else{
-    rej("wrong password")
-  }
-})
-  promise.then(function(res){
-  img.src = "https://media.tenor.com/UVmpVqlpVhQAAAAM/yess-yes.gif";
-})
-  .catch(function(err){
-  console.log(err)
-  img.src = "https://media.tenor.com/vLK4Mq3jiKIAAAAM/cat-no.gif";
- })
+let api = "https://dummyjson.com/products";
+let realData;
+
+const getData = async()=>{
+let res = await fetch(api)
+let data = await res.json()
+realData = data.products;
+display(realData)
+
 }
-function Delayfun(){
-img.src="https://loading.io/assets/mod/spinner/default/lg.gif"
-setTimeout(Checkfun, 2000);
+getData()
+
+const display = (data)=>{
+ main.innerHTML = ""
+ data.map((el,index)=>{
+ let name = document.createElement("h2");
+ name.innerText = el.title;
+ let p = document.createElement("h3");
+ p.innerText = el.price;
+ let img = document.createElement("img");
+ img.setAttribute("class", "images")
+ img.src = el.images[0];
+ let div = document.createElement("div");
+
+ div.append(img, name, p);
+ main.append(div);
+
+})
 }
+const filterpr =()=>{
+
+if (filprice.value == "lth"){
+     realData.sort((a, b) => {
+       return a.price - b.price;
+     });
+}else if(filprice.value == "htl"){
+     realData.sort((a, b) => {
+       return b.price - a.price;
+     });
+}
+
+display(realData)
+}
+
+input.addEventListener("input", ()=>{
+  let x = event.target.value.toLowerCase()
+
+  const filtered = realData.filter((data)=>{
+  return data.title.toLowerCase().includes(x)
+
+     
+  })
+  display(filtered)
+  
+  
+})
+
