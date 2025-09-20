661f9fb26f111b4ff2448e4ac0deaefe082883a3
Shraddha More <moresatish305@gmail.com>
2025-09-20 14:27:45 +0530

    Debouncing/Fetch

--- index.js
+++ index.js
@@ -1,62 +1,38 @@
-let main = document.getElementById("main")
-let filprice = document.querySelector("select")
+let api = "http://www.omdbapi.com/?i=tt3896198&apikey=105fb6bc";
 let input = document.querySelector("input")
+let search_box = document.getElementById("search_box");
 
 
-let api = "https://dummyjson.com/products";
-let realData;
 
 const getData = async()=>{
-let res = await fetch(api)
-let data = await res.json()
-realData = data.products;
-display(realData)
-
-}
-getData()
-
-const display = (data)=>{
- main.innerHTML = ""
- data.map((el,index)=>{
- let name = document.createElement("h2");
- name.innerText = el.title;
- let p = document.createElement("h3");
- p.innerText = el.price;
- let img = document.createElement("img");
- img.setAttribute("class", "images")
- img.src = el.images[0];
- let div = document.createElement("div");
-
- div.append(img, name, p);
- main.append(div);
-
-})
-}
-const filterpr =()=>{
-
-if (filprice.value == "lth"){
-     realData.sort((a, b) => {
-       return a.price - b.price;
-     });
-}else if(filprice.value == "htl"){
-     realData.sort((a, b) => {
-       return b.price - a.price;
-     });
+    let x = input.value;
+    let res = await fetch(`${api}&s=${x}`)
+    let data = await res.json()
+    
+    display(data.Search)
 }
 
-display(realData)
-}
+ function Delay(){
+   setTimeout(getData,1000)
+ }
 
-input.addEventListener("input", ()=>{
-  let x = event.target.value.toLowerCase()
+function display(data){
+  search_box.innerHTML=""
+  data.forEach(({Title,Poster,Year})=>{
+  
 
-  const filtered = realData.filter((data)=>{
-  return data.title.toLowerCase().includes(x)
+    let title = document.createElement("h4")
+    title.innerText=Title
+    let year = document.createElement("h5")
+    year.innerText=Year
+    let img = document.createElement("img")
+    img.src=Poster
+    let div = document.createElement("div")
 
-     
+    div.append(img,title,year)
+
+    search_box.append(div)
   })
-  display(filtered)
-  
-  
-})
+}
+ 
 
